003b0f6546f733b3f7de4df7c9847df68d143806
David Cofer <david.cofer@dynetics.com>
2018-02-16 14:00:28 -0600

    Moved the code to generate the labels masks from the raw masks into the FCN code to consolidate it.

---
+++ generate_label_masks_for_dir.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import image_utils as iu
+import train_img_utils as tu
+import os
+
+color_img_dirs = ["C:/Work/PowerLineInspection/aeryon_database/Defective/Flashed Insulators/",
+                  "C:/Work/PowerLineInspection/aeryon_database/NonDefective/"
+                 ]
+
+#color_img_dirs = ["C:/Work/PowerLineInspection/aeryon_database/test/"]
+image_dir = "C:/Work/PowerLineInspection/aeryon_database/images/"
+mask_dir = "C:/Work/PowerLineInspection/aeryon_database/labels/"
+
+label_list, ignore_list = tu.createFlashedOnlyList(192) #, 128, 64
+out_img_width = 1280
+out_img_height = 960
+
+
+def process_dir(input_dir):
+  colored_images = iu.findFilesOfType(input_dir, ["_mask.JPG", "_mask.jpg"])
+
+  for img_name in colored_images:
+    base_img_name = img_name[:-9] + ".JPG"
+    print("Processing: " + input_dir + base_img_name)
+
+    already_proc_files = iu.findFilesContaining(image_dir, [img_name[:-9]])
+
+    if len(already_proc_files) <= 0:
+        if os.path.isfile(input_dir  + base_img_name):
+          print("Generating label mask")
+          # Load in image and pad it out so it is N x min_width, N x min_height
+          color_img = cv2.imread(input_dir + img_name)
+
+          # print(color_img.shape)
+          # mask = tu.generateColorLabelMask(color_img, label_list, 200)
+          mask, ignore_mask = tu.generateBWLabelMask(color_img, label_list, ignore_list, 0)
+
+          # Load up the original image
+          orig_img = cv2.imread(input_dir + base_img_name)
+          orig_img_without_ignore = ignore_mask & orig_img #cv2.bitwise_and(orig_img, ignore_mask)
+
+          # Save out original images
+          img_out_name = image_dir + base_img_name
+          cv2.imwrite(img_out_name, orig_img_without_ignore)
+          #img_ignore_name = image_dir + "ignore_" + base_img_name
+          #cv2.imwrite(img_ignore_name, ignore_mask)
+          label_out_name = mask_dir + base_img_name[:-4] + ".png"
+          cv2.imwrite(label_out_name, mask)
+          print("saved image: " + img_out_name)
+
+        else:
+          print("Missing: " + base_img_name)
+    else:
+        print("Skipping file already processed: " + base_img_name)
+
+
+for input_dir in color_img_dirs:
+  process_dir(input_dir)
---
+++ train_img_utils.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+#
+# utilities for checking and producing training images from labeled images.
+# v1.0.0
+#
+#
+'''
+## License
+
+  COPYRIGHT (C) 2016 NeuroRobotic Technologies, LLC
+  ALL RIGHTS RESERVED. This code is not for public use.
+
+'''
+
+# David Cofer
+# Initial Date: 11 July 2016
+# Last Updated: 11 July 2016
+# http://www.NeuroRoboticTech.com/
+
+import os
+import argparse
+import cv2
+import numpy as np
+import image_utils
+import copy
+import math
+import image_utils as iu
+
+import SegmentLabelType as SType
+
+
+# Creates the label list and maps my color codes to pascal voc
+def createLabelList():
+  offset = 10
+  good_insulator_label = SType.SegmentLabelType('good_insulator', np.array([0, 0, 128]), np.array([0,0,255]), offset, 192)
+
+  flashed_insulator_label = SType.SegmentLabelType('flashed_insulator', np.array([0, 128, 128]), np.array([255,0,255]), offset, 192)
+  flashed_label = SType.SegmentLabelType('flashed', np.array([128, 0, 0]), np.array([255,150,255]), offset, 128)
+  flashed_top_label = SType.SegmentLabelType('flashed_top', np.array([64, 0, 0]), np.array([255,225,255]), offset, 192)
+
+  #broken_insulator_label = SType.SegmentLabelType('broken_insulator', np.array([255,0,0]), np.array([255,0,0]), offset, 0)
+  #broken_label = SType.SegmentLabelType('broken', np.array([255,100,100]), np.array([255,100,100]), offset, 0)
+
+  contaminated_insulator_label = SType.SegmentLabelType('contaminated_insulator', np.array([255,220,0]), np.array([255,220,0]), offset, 192)
+  #contamination_label = SType.SegmentLabelType('contamination', np.array([255,255,150]), np.array([255,255,150]), offset, 64)
+
+  label_list = [good_insulator_label, flashed_insulator_label, flashed_label, flashed_top_label, contaminated_insulator_label]
+
+  return label_list
+
+
+def createIgnoreLabelList():
+  offset = 10
+
+  broken_insulator_label = SType.SegmentLabelType('broken_insulator', np.array([255, 0, 0]), np.array([255, 0, 0]),
+                                                  offset, 255)
+  broken_label = SType.SegmentLabelType('broken', np.array([255, 100, 100]), np.array([255, 100, 100]), offset, 255)
+
+  contamination_label = SType.SegmentLabelType('contamination', np.array([255, 255, 150]), np.array([255, 255, 150]),
+                                               offset, 255)
+
+  ignore_label = SType.SegmentLabelType('ignore', np.array([192, 224, 224]), np.array([255,255,255]), offset, 255)
+
+  label_list = [ignore_label, broken_insulator_label, broken_label, contamination_label]
+
+  return label_list
+
+
+def createFlashedOnlyList(good_insulator_num):
+  label_list = createLabelList()
+  ignore_list = createIgnoreLabelList()
+
+  return label_list, ignore_list
+
+
+def generateBWLabelMask(img, labels, ignore_labels, threshold):
+  # create a combined mask with all zeros the same size as image
+  # so we can combine all the masks together
+  combined_mask = np.zeros(img.shape[:2], np.uint8)
+  combined_ignore_mask = np.zeros(img.shape[:2], np.uint8)
+
+  # Go through and generate the mask for each color and add it to the
+  for label_type in labels:
+    label_type.generateBWMask(img, threshold)
+    combined_mask = combined_mask + label_type.bw_mask
+
+  for label_type in ignore_labels:
+    label_type.generateBWMask(img, threshold)
+    combined_ignore_mask = combined_ignore_mask + label_type.bw_mask
+
+  ignore_mask = cv2.bitwise_not(combined_ignore_mask)
+  ignore_mask_bgr = cv2.merge((ignore_mask, ignore_mask, ignore_mask)).astype(np.uint8)
+
+  return combined_mask, ignore_mask_bgr
+
+
+def generateColorLabelMaskFromBWMask(bw_mask, labels):
+  # create a combined mask with all zeros the same size as image
+  # so we can combine all the masks together
+  combined_mask = np.zeros(bw_mask.shape, np.uint8)
+
+  # Go through and generate the mask for each color and add it to the
+  for label_type in labels:
+    label_type.generateColorMaskFromBWMask(bw_mask)
+    combined_mask = combined_mask + label_type.color_mask
+
+  return combined_mask
+
+
